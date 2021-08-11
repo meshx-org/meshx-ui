aac591561c507003afa3854867f3b65e664665b0
Roland Sz. Kovács <roland@mesh-x.com>
2021-08-11 15:07:17 +0200

    added windows version of ControllGraphics

---
+++ src/components/shared/ControlGraphics.windows.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { FC } from 'react'
+
+interface IProps {
+    width: number
+}
+
+const ControlGraphics: FC<IProps> = ({ width }) => {
+    return null
+}
+
+export default ControlGraphics
