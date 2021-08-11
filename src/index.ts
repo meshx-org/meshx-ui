d77d89ac2d12822bf1847da674c71e647754466e
Roland Sz. Kovács <roland@mesh-x.com>
2021-08-11 18:32:24 +0200

    Added ControlGraphics to exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import Button from './components/shared/Button'
 import Input from './components/shared/Input'
 import Header from './components/header/Header'
+import ControlGraphics from './components/shared/ControlGraphics'
 
-export { Button, Input, Header }
+export { Button, Input, Header, ControlGraphics }
