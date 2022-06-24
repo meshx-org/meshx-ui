c4f7862d52b3f1a600b555456139f07f73f6bdfc
Sz. Kovács Roland <Roland.Szarka-Kovacs@meshx.co>
2022-06-24 22:41:12 +0200

    export next button

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
-import Button from './components/button/Button'
 import Input from './components/input/TextField'
 import ControlSurface from './components/surface/ControlSurface'
 import { Card } from './components/card/Card'
 
-export { Button, Input, ControlSurface, Card }
+export { Input, ControlSurface, Card }
 
-export { default as ButtonX } from './components/next/button/Button'
+export { default as Button } from './components/next/button/Button'
 export { TextBox } from './components/next/text-box/TextBox'
 export { Person } from './components/next/person/Person'
 export { Branding } from './components/next/branding/Branding'
