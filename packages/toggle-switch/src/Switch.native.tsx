b50fd365cdd4a76b6d06709bfe1bfe4753f4228c
Sz. Kovács Roland <Roland.Szarka-Kovacs@meshx.co>
2022-11-12 14:14:47 +0100

    fix switch component build issue on ci

--- packages/toggle-switch/src/Switch.native.tsx
+++ packages/toggle-switch/src/Switch.native.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
-import { Switch as RNSwitch } from 'react-native'
 import { SwitchProps } from './Switch.types'
 
 export function Switch(props: SwitchProps) {
-    return <RNSwitch />
+    return null
 }
