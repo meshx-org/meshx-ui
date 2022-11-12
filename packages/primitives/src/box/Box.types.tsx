ecbacb61a225255a0e14a89708aad3ca1e24fe3f
Sz. Kovács Roland <Roland.Szarka-Kovacs@meshx.co>
2022-11-12 14:32:03 +0100

    improve monorepo deps

--- packages/primitives/src/box/Box.types.tsx
+++ packages/primitives/src/box/Box.types.tsx
@@ -1,5 +1,6 @@
+import React from "react"
 import { SpaceProps, LayoutProps, ColorProps, BorderProps, PositionProps, BackgroundProps } from 'styled-system'
 
 export interface BoxProps extends SpaceProps, ColorProps, LayoutProps, BackgroundProps, BorderProps, PositionProps {
-    children?: React.ReactNode | React.ReactNode[] | unknown
+    children?: React.ReactNode | React.ReactNode[]
 }
