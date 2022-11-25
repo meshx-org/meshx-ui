9eb1d6d383d4288e9a93af40fb3ca851772713e3
Sz. Kovács Roland <Roland.Szarka-Kovacs@meshx.co>
2022-11-25 19:26:51 +0100

    fix zIndex issue in overlay

--- packages/overlays/src/Overlay.tsx
+++ packages/overlays/src/Overlay.tsx
@@ -1,3 +1,15 @@
+/*
+ * Copyright 2020 Adobe. All rights reserved.
+ * This file is licensed to you under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy
+ * of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+ * OF ANY KIND, either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+
 import React, { useState, useMemo } from 'react'
 import ReactDOM from 'react-dom'
 import { OverlayProps, OverlayAriaProps } from './Overlay.types'
@@ -42,7 +54,7 @@ function Overlay(props: OverlayProps, ref: Ref<HTMLDivElement>) {
             opacity: isOpen ? 1 : 0,
             transform: isOpen ? 'translateY(0px)' : 'translateY(6px)'
         },
-        config: {duration: 100, ...config.default}
+        config: { duration: 100, ...config.default }
     })
 
     // Don't un-render the overlay while it's transitioning out.
@@ -54,7 +66,7 @@ function Overlay(props: OverlayProps, ref: Ref<HTMLDivElement>) {
 
     return (
         <OverlayAria portalContainer={container}>
-            <div style={{ position: 'relative', background: 'transparent', isolation: 'isolate' }}>
+            <div style={{ zIndex: 99999, background: 'transparent', isolation: 'isolate' }}>
                 <div ref={ref} style={overlayStyles}>
                     <animated.div style={styles}>{children}</animated.div>
                 </div>
